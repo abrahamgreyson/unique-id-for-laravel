09615ed8e48f5ad985fae2701ca8bcf9f1e64a9e
abrahamgreyson <abrahamgreyson@users.noreply.github.com>
2023-02-17 18:21:37 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -20,7 +20,7 @@ class TestCase extends Orchestra
     protected function getPackageProviders($app)
     {
         return [
-            UniqueIdServiceProvider::class
+            UniqueIdServiceProvider::class,
         ];
     }
 
